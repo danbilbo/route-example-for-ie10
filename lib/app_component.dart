e21afd416f13a4774e953ca1524a466574350079
dselivanov <daniil@yoxel.com>
2016-04-11 12:50:10 +0300

    init

---
+++ lib/app_component.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/angular2.dart';
+import 'package:angular2/router.dart';
+
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    providers: const [
+      ROUTER_PROVIDERS,
+      const Provider(APP_BASE_HREF, useValue: '/'),
+      const Provider(LocationStrategy, useClass: HashLocationStrategy),
+    ],
+    directives: const [
+      ROUTER_DIRECTIVES]
+
+)
+@RouteConfig(const [
+  const Route(path: '/a',
+      component: AComponent,
+      useAsDefault: true,
+      name: 'ARoute'),
+  const Route(path: '/b',
+      component: BComponent,
+      name: 'BRoute'),
+  const Route(path: '/c',
+      component: CComponent,
+      name: 'CRoute'),
+])
+class AppComponent {}
+
+@Component(selector: 'a-cmp', template: '<p>A Route</p>')
+class AComponent {
+}
+
+@Component(selector: 'b-cmp', template: '<p>B Route</p>')
+class BComponent {
+}
+
+@Component(selector: 'c-cmp', template: '<p>C Route</p>')
+class CComponent {
+}
